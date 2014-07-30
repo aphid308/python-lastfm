0e6d75ca84ceb72b652f68072825ac4540ac05b7
Kill Face <killface@TheAnnihilatrix.(none)>
2014-07-29 21:02:48 -0400

    initial commit

---
+++ common.py
@@ -0,0 +1,63 @@
+import requests
+
+
+class Artist(object):
+    baseurl = "http://ws.audioscrobbler.com/2.0/"
+
+    def __init__(self, artist, api_key):
+        self.payload = {'artist': artist, 'api_key': api_key, 'format': 'json'}
+
+    def get_events(self, limit=5):
+        self.payload.update({'method': 'artist.getevents', 'limit': limit})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+        
+    @property
+    def get_info(self):
+        self.payload.update({'method': 'artist.getinfo'})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    def get_passed_events(self, limit=5):
+        self.payload.update({'method': 'artist.getpastevents', 'limit': limit})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    @property
+    def get_podcast(self):
+        self.payload.update({'method': 'artist.getpodcast'})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    def get_shouts(self, limit=5):
+        self.payload.update({'method': 'artist.getshouts', 'limit': limit})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    def get_similar(self, limit=5):
+        self.payload.update({'method': 'artist.getsimilar', 'limit': limit})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    def get_usertags(self, user='RJ'):
+        self.payload.update({'method': 'artist.gettags', 'user': user})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    def get_topalbums(self, limit=5):
+        self.payload.update({'method': 'artist.gettopalbums', 'limit': limit})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+    
+    @property
+    def get_topfans(self):
+        self.payload.update({'method': 'artist.gettopfans'})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
+    @property
+    def get_toptags(self):
+        self.payload.update({'method': 'artist.gettoptags'})
+        api_call = requests.get(self.baseurl, params=self.payload)
+        return api_call.json()
+
---
+++ tests.py
@@ -0,0 +1,7 @@
+import common
+
+r = common.Artist('cher', '8ed0103c4c47b623fb344b83c3c2c222')
+
+l = r.get_events()
+
+print l
