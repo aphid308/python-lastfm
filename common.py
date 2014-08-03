d787c53b6ab423a195c30b0edb424d4adc3bb578
aphid308 <patrick.a.jury@gmail.com>
2014-08-02 22:23:02 -0400

    Update common.py
    refactor

--- common.py
+++ common.py
@@ -7,60 +7,51 @@ class Artist(object):
     def __init__(self, artist, api_key):
         self.payload = {'artist': artist, 'api_key': api_key, 'format': 'json'}
     
-    def get(self, url=self.baseurl, params):
+    def get(self, url=self.baseurl, params=self.payload):
         api_call = requests.get(url, params=params)
         return api_call.json()
     
     def get_events(self, limit=5):
         self.payload.update({'method': 'artist.getevents', 'limit': limit})
-        self.get(params=self.payload)
+        return self.get()
         
     @property
     def get_info(self):
         self.payload.update({'method': 'artist.getinfo'})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        self.get()
 
     def get_passed_events(self, limit=5):
         self.payload.update({'method': 'artist.getpastevents', 'limit': limit})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     @property
     def get_podcast(self):
         self.payload.update({'method': 'artist.getpodcast'})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     def get_shouts(self, limit=5):
         self.payload.update({'method': 'artist.getshouts', 'limit': limit})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     def get_similar(self, limit=5):
         self.payload.update({'method': 'artist.getsimilar', 'limit': limit})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     def get_usertags(self, user='RJ'):
         self.payload.update({'method': 'artist.gettags', 'user': user})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     def get_topalbums(self, limit=5):
         self.payload.update({'method': 'artist.gettopalbums', 'limit': limit})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
     
     @property
     def get_topfans(self):
         self.payload.update({'method': 'artist.gettopfans'})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
     @property
     def get_toptags(self):
         self.payload.update({'method': 'artist.gettoptags'})
-        api_call = requests.get(self.baseurl, params=self.payload)
-        return api_call.json()
+        return self.get()
 
